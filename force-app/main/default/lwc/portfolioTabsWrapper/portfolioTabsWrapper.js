b90ca22b7d66f06f53180a2d6a7211f9b12fcd12
Owner <vikaskumar.sfdx@gmail.com>
2026-05-24 11:23:26 +0530

    Third Commit

---
+++ force-app/main/default/lwc/portfolioTabsWrapper/portfolioTabsWrapper.js
@@ -0,0 +1,7 @@
+import { LightningElement, api } from 'lwc';
+
+export default class PortfolioTabsWrapper extends LightningElement {
+
+    @api objectApiName //= 'Protfolio__c'
+    @api recordId //= 'a09NS00000TTyT7YAL';
+}
